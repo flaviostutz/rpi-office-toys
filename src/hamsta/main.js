e37f8b57980a9284fc02c6fc7f2d0802e2648611
Flavio Stutz <stutz@bb.com.br>
2016-02-16 12:06:54 +0000

    First release of Hamsta

--- src/hamsta/main.js
+++ src/hamsta/main.js
@@ -2,13 +2,13 @@
 
 var HamstaExtension = require("./HamstaExtension.js");
 
-var hamstaExtension = new HamstaExtension("mqtt://test.mosquitto.org:1883", "1/devices/1/1/", 1000);
+var hamstaExtension = new HamstaExtension("mqtt://test.mosquitto.org:1883", "1/devices/1/1/", 1000, 0.062);
 
 hamstaExtension.on("step", function() {
-  console.log("Stepped in " + hamstaExtension.getLastStepElapsedTime() + "ms");
+//  console.log("Stepped in " + hamstaExtension.getLastStepElapsedTime() + "ms");
 });
 
 hamstaExtension.on("initialized", function() {
   console.log("HAMSTA INITIALIZED!");
-  hamstaExtension.start(2);
+  hamstaExtension.start(5);
 });
